71cba57aaf41659ded2073f1015d227267ea25d2
ivanSilvaLI <ivansilira@outlook.com>
2023-03-21 17:28:50 -0300

    conversão de medidas

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function calculo() {
+    let calcular = document.getElementById("calcular").value
+    let valor = parseFloat(document.getElementById("valor").value)
+
+    //  if (calcular = 'c/m'){
+    //      resultado = valor * 100
+    //  }
+    //  else if (calcular = 'm/c'){
+    //      resultado = valor / 100
+    //  }
+    //  else if (calcular = 'k/l'){
+    //      resultado = valor *  2.2046
+    //  }
+    // else{
+    //      resultado = valor / 2.2046
+    //  }
+
+    switch (calcular) {
+        case "c/m":
+            resultado = valor / 100
+            break
+        case "m/c":
+            resultado = valor * 100
+            break
+        case "k/l":
+            resultado = valor * 2.2046
+            break
+        case "l/k":
+            resultado = valor / 2.2046
+    }
+
+
+    document.getElementById("resultado").value = resultado;
+
+}
